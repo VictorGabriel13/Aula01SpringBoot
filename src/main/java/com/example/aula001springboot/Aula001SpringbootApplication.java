dae7618506c0327fc41d5f4641267c6755ed4a0f
Victor Gabriel de Lima Rocha <20241204010013@ifrn.local>
2026-08-11 09:22:51 -0300

    Primeiro projeto usando Spring

---
+++ src/main/java/com/example/aula001springboot/Aula001SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.aula001springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula001SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula001SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aula001springboot/Aula001SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula001springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula001SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
