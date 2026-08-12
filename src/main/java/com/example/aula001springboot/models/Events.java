21da6d10015212b5b3a5505e7b97eb11bf56af18
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2026-08-12 19:49:06 -0300

    Configurando classe de modelo

--- src/main/java/com/example/aula001springboot/models/Events.java
+++ src/main/java/com/example/aula001springboot/models/Events.java
@@ -1,13 +1,27 @@
 package com.example.aula001springboot.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
 public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String local;
     private LocalDateTime hour;
     private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
