e7f1a7e772fd3c80dc622a5f290ad97f8ae948c1
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2026-08-31 23:19:15 -0300

    Criando listagem de Eventos

--- src/main/java/com/example/aula001springboot/controllers/EventsController.java
+++ src/main/java/com/example/aula001springboot/controllers/EventsController.java
@@ -4,24 +4,38 @@ import com.example.aula001springboot.models.Events;
 import com.example.aula001springboot.repositories.EventsRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/events")
 public class EventsController {
     @Autowired
     private EventsRepositories er;
 
-    @RequestMapping("/events/form")
+    @RequestMapping("/form")
     public String form() {
         return "events/formEvents";
     }
    // @RequestMapping(path = "/events", method = RequestMethod.POST)
-    @PostMapping("/events")
+    @PostMapping
     public String adicionar(Events events){
 
         System.out.println(events);
         er.save(events);
         return "events/events-add";
+    }
+
+    @GetMapping
+    public ModelAndView listar() {
+
+        List<Events> eventos = er.findAll();
+        ModelAndView mv = new ModelAndView("events/lista");
+        mv.addObject("events", eventos);
+        return mv;
     }
 }
