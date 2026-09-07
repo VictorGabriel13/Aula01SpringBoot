81ba546f5b23712a50cd923e3abe8822d77b854b
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2026-09-07 20:26:59 -0300

    Criando a página de detalhes do evento

--- src/main/java/com/example/aula001springboot/controllers/EventsController.java
+++ src/main/java/com/example/aula001springboot/controllers/EventsController.java
@@ -5,11 +5,13 @@ import com.example.aula001springboot.repositories.EventsRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/events")
@@ -36,6 +38,24 @@ public class EventsController {
         List<Events> eventos = er.findAll();
         ModelAndView mv = new ModelAndView("events/lista");
         mv.addObject("events", eventos);
+        return mv;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+        ModelAndView mv = new ModelAndView();
+        Optional<Events> opt = er.findById(id);
+
+        if (opt.isEmpty()) {
+            mv.setViewName("redirect:/events");
+            return mv;
+        }
+
+        Events event = opt.get();
+
+        mv.setViewName("/events/detalhes");
+        mv.addObject("event", event);
+
         return mv;
     }
 }
